a9a5f9a89f78ad028246568ba87cd772830380c4
luckydevn16 <luckydevn16@gmail.com>
2023-08-04 08:35:41 +0800

    Updates

---
+++ static/1690855981/movies/667538/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fmovie-app\u002Fstatic\u002F1690855981",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F667538",config:{_app:{basePath:b,assetsPath:b,cdnURL:a}}}}(null,"\u002Fmovie-app\u002F"));
